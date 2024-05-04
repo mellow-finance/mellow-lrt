d6df3903e34f3e5a08ecc4fda8c6c4f8d62c65e1
Andrey Borzenkov <armoking32@gmail.com>
2024-05-05 03:02:46 +0400

    mocks

---
+++ test/mainnet/unit/strategies/DefaultBondStrategyTest.t.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: BSL-1.1
+pragma solidity ^0.8.0;
+
+import "../../Constants.sol";
+
+contract Unit is Test {
+    using SafeERC20 for IERC20;
+
+    function testConstructor() external {}
+}
