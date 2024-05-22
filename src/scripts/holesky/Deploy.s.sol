b181d8e863c79e19c5da159c1b56620732c70298
Andrey Borzenkov <armoking32@gmail.com>
2024-05-22 06:09:19 +0400

    deploy script: init

---
+++ src/scripts/holesky/Deploy.s.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: BSL-1.1
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+import "forge-std/Script.sol";
+import "forge-std/Vm.sol";
+
+import "../../Vault.sol";
+
+contract Deploy is Script {
+    function deployOracles() public {}
+    function deployValidators() public {}
+    function deployModules() public {}
+    function deployStrategies() public {}
+    function deployVault() public {}
+
+    function run() external {}
+}
