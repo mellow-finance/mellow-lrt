04e905bdd4187e2b90a39129ca0d406482a5bbaa
Andrey Borzenkov <armoking32@gmail.com>
2024-05-29 14:56:09 +0400

    More deploy script logic added

---
+++ tests/mainnet/e2e/IDeploy.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: BSL-1.1
+pragma solidity 0.8.25;
+
+import "../Constants.sol";
+
+interface IDeploy {
+    struct DeployParameters {
+        address deployer;
+        address vaultAdmin;
+        address vaultCurator;
+        address proposer;
+        address acceptor;
+        address emergencyOperator;
+        address baseImplementation;
+        address wstethDefaultBond;
+        uint256 maximalTotalSupply;
+        string lpTokenName;
+        string lpTokenSymbol;
+    }
+
+    struct DeploySetup {
+        Vault vault;
+        IVaultConfigurator configurator;
+        ERC20TvlModule erc20TvlModule;
+        DefaultBondTvlModule defaultBondTvlModule;
+        DefaultBondModule defaultBondModule;
+        ManagedValidator validator;
+        ManagedRatiosOracle ratiosOracle;
+        ChainlinkOracle priceOracle;
+        DefaultBondStrategy defaultBondStrategy;
+        DepositWrapper depositWrapper;
+        DefaultProxyImplementation defaultProxyImplementation;
+        AdminProxy adminProxy;
+        IAggregatorV3 wethAggregatorV3;
+        IAggregatorV3 wstethAggregatorV3;
+    }
+}
