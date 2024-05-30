53fe6e179358742f6a38294234e70a850c9ca33f
aiants <aiants@nvidia.com>
2024-05-30 14:32:39 +0400

    removed line

--- src/utils/DefaultAccessControl.sol
+++ src/utils/DefaultAccessControl.sol
@@ -16,6 +16,7 @@ contract DefaultAccessControl is
     /// @param admin Admin of the contract
     constructor(address admin) {
         if (admin == address(0)) revert AddressZero();
+
         _grantRole(OPERATOR, admin);
         _grantRole(ADMIN_ROLE, admin);
 
